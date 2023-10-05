bc4aec18ad683cf8d729edede6b4a78035ac5bef
mustafa <mustafamcm9@gmail.com>
2023-10-05 20:48:55 +0300

    part 1 | Exercise unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={props.good}></StatisticLine>
+        <StatisticLine text="neutral" value={props.neutral}></StatisticLine>
+        <StatisticLine text="bad" value={props.bad}></StatisticLine>
+        <StatisticLine text="all" value={props.all}></StatisticLine>
+        <StatisticLine text="average" value={props.average}></StatisticLine>
+        <StatisticLine
+          text="positive"
+          value={props.positivePercentage}
+        ></StatisticLine>
+      </tbody>
+    </table>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [positivePercentage, setPostivePercentage] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleGood = () => {
+    const updatedGood = good + 1;
+    const updatedAll = all + 1;
+    setGood(updatedGood);
+    setAll(updatedAll);
+    handleAverage(updatedGood, bad, updatedAll);
+    handlePositivePercantage(updatedGood, updatedAll);
+  };
+
+  const handleBad = () => {
+    const updatedBad = bad + 1;
+    const updatedAll = all + 1;
+    setBad(updatedBad);
+    setAll(updatedAll);
+    handleAverage(good, updatedBad, updatedAll);
+    handlePositivePercantage(good, updatedAll);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+    handlePositivePercantage(good, all + 1);
+  };
+
+  const handleAverage = (updatedGood, updatedBad, updatedAll) => {
+    const score = updatedGood - updatedBad;
+    setAverage(score / updatedAll);
+  };
+
+  const handlePositivePercantage = (updatedGood, updatedAll) => {
+    const percantage = (updatedGood / updatedAll) * 100;
+    setPostivePercentage(percantage);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={handleGood}></Button>
+      <Button text="neutral" handleClick={handleNeutral}></Button>
+      <Button text="bad" handleClick={handleBad}></Button>
+      <h1>statistics</h1>
+      <Statistics
+        good={good}
+        bad={bad}
+        neutral={neutral}
+        average={average}
+        all={all}
+        positivePercentage={positivePercentage}
+      ></Statistics>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
