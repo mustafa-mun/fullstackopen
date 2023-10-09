412e4ba03d4503a1091c3b2608099468187cc9fe
mustafa <mustafamcm9@gmail.com>
2023-10-09 22:54:42 +0300

    part2 | Exercise: phonebook - Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ newFilter, handleFilterChange }) => {
   return (
@@ -41,16 +42,20 @@ const Persons = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newNumber, setNewNumber] = useState("");
   const [newName, setNewName] = useState("");
   const [newFilter, setNewFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
