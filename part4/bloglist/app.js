175b0fbd2aab6a55b1254386204eb8fbf156c503
mustafa <mustafamcm9@gmail.com>
2023-10-18 22:13:42 +0300

    part4 | Exercise: bloglist   Update middleware logic: Now only certain routes uses the userExtractor

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -15,7 +15,6 @@ mongoose.connect(config.MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 app.use(middleware.tokenExtractor);
-app.use(middleware.userExtractor);
 
 app.use("/api/blogs", blogsController);
 app.use("/api/users", usersController);
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const middleware = require("../utils/middleware");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
@@ -12,40 +13,48 @@ blogsRouter.get("/", async (request, response, next) => {
   }
 });
 
-blogsRouter.post("/", async (request, response, next) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "invalid token" });
-  }
-  const user = await User.findById(decodedToken.id);
-  const blog = new Blog({ ...request.body, user });
-
-  if (!blog.url || !blog.title) {
-    return response.status(400).json({ error: "title and url are required" });
-  }
-
-  try {
-    const savedBlog = await blog.save();
-    user.blogs.push(savedBlog);
-    await user.save();
-    response.status(201).json(savedBlog);
-  } catch (error) {
-    next(error);
-  }
-});
-
-blogsRouter.delete("/:id", async (request, response, next) => {
-  try {
-    const blog = await Blog.findById(request.params.id);
-    if (blog.user.toString() !== request.user._id.toString()) {
-      return response.status(403).json({ error: "unauthorized" });
+blogsRouter.post(
+  "/",
+  middleware.userExtractor,
+  async (request, response, next) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "invalid token" });
     }
-    await Blog.deleteOne({ _id: blog._id });
-    response.status(204).end();
-  } catch (error) {
-    next(error);
+    const user = await User.findById(decodedToken.id);
+    const blog = new Blog({ ...request.body, user });
+
+    if (!blog.url || !blog.title) {
+      return response.status(400).json({ error: "title and url are required" });
+    }
+
+    try {
+      const savedBlog = await blog.save();
+      user.blogs.push(savedBlog);
+      await user.save();
+      response.status(201).json(savedBlog);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
+
+blogsRouter.delete(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response, next) => {
+    try {
+      const blog = await Blog.findById(request.params.id);
+      if (blog.user.toString() !== request.user._id.toString()) {
+        return response.status(403).json({ error: "unauthorized" });
+      }
+      await Blog.deleteOne({ _id: blog._id });
+      response.status(204).end();
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 blogsRouter.put("/:id", async (request, response, next) => {
   try {
